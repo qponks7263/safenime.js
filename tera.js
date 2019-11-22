cce19a9c399e94d598d196edd4408b1456a320aa
qponks7263 <57935458+qponks7263@users.noreply.github.com>
2019-11-23 04:29:46 +0700

    Create tera.js

---
+++ tera.js
@@ -0,0 +1,10 @@
+<script type="text/javascript">
+	atOptions = {
+		'key' : 'b3158ad0236a8dab89660d2c7985efe2',
+		'format' : 'iframe',
+		'height' : 50,
+		'width' : 320,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.bcloudhost.com/b3158ad0236a8dab89660d2c7985efe2/invoke.js"></scr' + 'ipt>');
+</script>
